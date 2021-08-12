5856d75f6b2df8e51c909e35645d84709ac9bf64
Pratha yadav <83515666+Prathayadav@users.noreply.github.com>
2021-08-12 18:02:34 +0530

    Update signup_db.js

--- signup_db.js
+++ signup_db.js
@@ -1,1 +1,63 @@
+var MongoClient = require('mongodb').MongoClient;
+var express =require('express');
+const path = require('path');
+app = express();
+var bodyParser = require('body-parser');
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded(
+    {
+        extended:false
+    }
+));
+
+const port = 4000;
+
+app.use(bodyParser.json());
+
+app.get('/',function(req,res)
+{
+  
+});
+
+app.post('/',function(req,res)
+{
+    
+    var email=req.body.email;
+    var password=req.body.password;
+    var address=req.body.address;
+    var contact_no=req.body.contact_no;
+    ConnectionToMongoDBForLoginValidation(email,password,address,contact_no);
+    res.send("You are successfully registered,please Login,")
+    
+    
+   
+    
+  
+
+});
+function ConnectionToMongoDBForLoginValidation(email_,password_,address_,contact_no_)
+    {
+        MongoClient.connect('mongodb://localhost:27017/pizzawebsite',function(err,client)
+        {
+            if(err) throw err;
+            else 
+            console.log("connecteddddd to mongodb.....");
+    
+            var data = client.db('pizzawebsite');
+            data.collection("users").insertOne({email:email_,password:password_,address:address_,contact_no:contact_no_},
+                function(err,result)
+                {
+                    if(err) res.send(err);
+                    
+                    console.log(result);
+                   
+                });
+    
+        });
+               
+    
+    }
+app.listen(port);
+
 
